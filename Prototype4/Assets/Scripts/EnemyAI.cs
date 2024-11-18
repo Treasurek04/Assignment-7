fd3920a49b8a853d93177fa7b87bc52be8e1a7f7
Treasurek04 <treasurekeys2004@gmail.com>
2024-11-18 01:01:17 -0600

    creating scripts

--- Prototype4/Assets/Scripts/EnemyAI.cs
+++ Prototype4/Assets/Scripts/EnemyAI.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+
+    public Rigidbody enemyRb;
+    public GameObject player;
+    public float speed = 3.0f; 
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 lookingDirection = (player.transform.position - transform.position).normalized;
+
+        enemyRb.AddForce(lookingDirection * speed);
     }
 }
